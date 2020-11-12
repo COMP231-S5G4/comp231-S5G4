84c1937772aa94d4e4fbf805a336973e620868bb
JustinMuere <jmuere@my.centennialcollege.ca>
2020-11-12 16:50:19 -0500

    updated ui colours

--- app/src/main/java/com/comp231_s5g4/instabod/UserProfileActivity.java
+++ app/src/main/java/com/comp231_s5g4/instabod/UserProfileActivity.java
@@ -3,6 +3,7 @@ package com.comp231_s5g4.instabod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+    public void onClickButton(View view){
+
     }
 }
