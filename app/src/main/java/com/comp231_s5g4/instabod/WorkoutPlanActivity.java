8e329a28091bc04cbaf905b15a8efd1d0fd3db42
Dhruv <dhruvbandaria@hotmail.com>
2020-11-20 12:25:56 -0500

    Fixed String.xml Files

--- app/src/main/java/com/comp231_s5g4/instabod/WorkoutPlanActivity.java
+++ app/src/main/java/com/comp231_s5g4/instabod/WorkoutPlanActivity.java
@@ -3,6 +3,9 @@ package com.comp231_s5g4.instabod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class WorkoutPlanActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class WorkoutPlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_plan);
+
     }
 }
