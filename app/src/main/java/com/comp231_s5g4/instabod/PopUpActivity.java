ceb934b0b3263db6feea345d478e7e6c4d0a53de
Xin-Rong Lin <XeonDigital>
2020-11-21 03:22:11 -0500

    Linked the algorithm to the ui

--- app/src/main/java/com/comp231_s5g4/instabod/PopUpActivity.java
+++ app/src/main/java/com/comp231_s5g4/instabod/PopUpActivity.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class PopUpActivity extends AppCompatActivity {
 
-    TextView heading;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up);
-
-        heading = (TextView)findViewById(R.id.exerciseTitleTextview);
+        //getting all the views
+        TextView heading = findViewById(R.id.exerciseTitleTextview);
+        //workout1
+        TextView name1 = findViewById(R.id.exerciseName_Textview);
+        TextView reps1 = findViewById(R.id.exerciseReps_Textview);
+        TextView hyperlink1 = findViewById(R.id.hyperlink_Textview);
+        //workout2
+        TextView name2 = findViewById(R.id.exerciseName2_Textview);
+        TextView reps2 = findViewById(R.id.exerciseReps2_Textview);
+        TextView hyperlink2 = findViewById(R.id.hyperlink2_Textview);
+        //workout3
+        TextView name3 = findViewById(R.id.exerciseName3_Textview);
+        TextView reps3 = findViewById(R.id.exerciseReps3_Textview);
+        TextView hyperlink3 = findViewById(R.id.hyperlink3_Textview);
 
         Intent intent = getIntent();
         String title = intent.getStringExtra("dayTitle");
@@ -27,9 +39,24 @@ public class PopUpActivity extends AppCompatActivity {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-         int width = dm.widthPixels;
-         int height = dm.heightPixels;
-
-         getWindow().setLayout((int)(width*.8),(int)(height*.5));
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width * .8), (int) (height * .6));
+        //setting the workouts
+        Bundle bundle = intent.getExtras();
+        if(bundle != null){
+            //workout1
+            name1.setText(name1.getText() + bundle.getString("workout1Name"));
+            reps1.setText(reps1.getText() + Integer.toString(bundle.getInt("workout1Reps")));
+            hyperlink1.setText(hyperlink1.getText() + bundle.getString("workout1HyperLink"));
+            //workout2
+            name2.setText(name2.getText() + bundle.getString("workout2Name"));
+            reps2.setText(reps2.getText() + Integer.toString(bundle.getInt("workout2Reps")));
+            hyperlink2.setText(hyperlink2.getText() + bundle.getString("workout2HyperLink"));
+            //workout3
+            name3.setText(name3.getText() + bundle.getString("workout3Name"));
+            reps3.setText(reps3.getText() + Integer.toString(bundle.getInt("workout3Reps")));
+            hyperlink3.setText(hyperlink3.getText() + bundle.getString("workout3HyperLink"));
+        }
     }
 }
