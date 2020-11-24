7990c036787289f0aa8974adac8692033f92972c
Dhruv <dhruvbandaria@hotmail.com>
2020-11-24 18:54:56 -0500

    Fixed User Login Validation

--- app/src/main/java/com/comp231_s5g4/instabod/LoginActivity.java
+++ app/src/main/java/com/comp231_s5g4/instabod/LoginActivity.java
@@ -2,6 +2,7 @@ package com.comp231_s5g4.instabod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +16,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText PasswordEditText;
     Button LoginButton;
 
+    WorkoutUserManager db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db= new WorkoutUserManager(getApplicationContext());
+
         LoginButton = findViewById(R.id.loginButton);
         UsernameEditText = findViewById(R.id.usernameText);
         PasswordEditText = findViewById(R.id.editWaistText);
@@ -27,14 +32,34 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = UsernameEditText.getText().toString();
                 String password = PasswordEditText.getText().toString();
-
+                WorkoutUser workoutUser = new WorkoutUser();
                 Boolean isError=true;
                 if(username.equals("") || password.equals("")){
                     isError=false;
                 }
-
                 if(isError){
-                    //Valid Data
+                    int convertedUsername=WorkoutUser.convertUsernameToUniqueId(username);
+                    try {
+                        workoutUser=db.getWorkoutUserById(convertedUsername,"username");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    if(workoutUser==null){
+                        UsernameEditText.setTextColor(Color.RED);
+                        Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        if(password.equals(workoutUser.getPassword())){
+                            // Username & password Valid
+                            // TODO - Enter the Shared Preference and move to next activity
+                            UsernameEditText.setTextColor(Color.BLACK);
+                            Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            UsernameEditText.setTextColor(Color.RED);
+                            Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG).show();
+                        }
+                    }
                 }
                 else{
                     if(username.equals("")){
