e085466fda5694235ee74eef0d099b9cb6927d45
Al Hafeez <aragornlotr954@gmail.com>
2020-11-30 19:54:46 -0500

    Added UI for Admin Home Page

--- app/src/main/java/com/comp231_s5g4/instabod/AdminHomePage.java
+++ app/src/main/java/com/comp231_s5g4/instabod/AdminHomePage.java
@@ -3,10 +3,12 @@ package com.comp231_s5g4.instabod;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AdminHomePage extends AppCompatActivity {
+public class AdminHomePage extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
     }
